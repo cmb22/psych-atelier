edac3475c36e6a6ff46436929254727aaf6caf43
chris <chris@MacBook-Pro-2.local>
2026-09-04 19:03:43 +0200

    black link

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/en/the-67-in-black",
+        destination: "https://www.etsy.com/de/listing/4561798676/der-67-in-schwarz-auto-mantel-aus",
+        permanent: false,
+      },
+    ];
+  },
 };
-
 export default nextConfig;
